7c3026a8b070b44ba680d3c63df41fbe8b710abc
lemueljohnperez0 <lemueljohnperez0@gmail.com>
2024-04-12 00:09:46 +0800

    Modified contact functionality

--- public/js/SendMail.js
+++ public/js/SendMail.js
@@ -1,42 +1,43 @@
 const contactForm = document.querySelector('#form');
-let name = document.getElementById('name');
-let email = document.getElementById('email');
-let contact = document.getElementById('contact');
-let subject = document.getElementById('subject');
-let message = document.getElementById('message');
 
 contactForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    let formData = {
-        name: name.value,
-        email: email.value,
-        contact: contact.value,
-        subject: subject.value,
-        message: message.value
-    }
+    const formData = {
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        contact: document.getElementById('contactNo').value,
+        subject: document.getElementById('subject').value,
+        message: document.getElementById('message').value
+    };
 
     console.log(formData);
-    
-    let xhr = new XMLHttpRequest();
-    xhr.open('POST', '/');
-    xhr.setRequestHeader('content-type', 'application/json');
-    xhr.onload = function() {
-        console.log(xhr.responseText);
-        if(xhr.responseText == 'Email sent successfully') {
-            alert("Email sent successfully!");
-            name.value = '';
-            email.value = '';
-            contact.value = '';
-            subject.value = '';
-            message.value = ''
-        }
 
-        else {
-            alert("Something went wrong.");
+    fetch('/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+    })
+    .then(response => response.text())
+    .then(data => {
+        console.log(data);
+        if(data === 'Email sent successfully') {
+            alert('Email sent');
+            document.getElementById('name').value = '';
+            document.getElementById('email').value = '';
+            document.getElementById('contactNo').value = '';
+            document.getElementById('subject').value = '';
+            document.getElementById('message').value = '';
         }
-    }
-    
-    xhr.send(JSON.stringify(formData));
-    
-})
+        
+        else {
+            alert('Something went wrong.');
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('Something went wrong.');
+    });
+});
